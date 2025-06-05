9d0774793564ff1f7d94ac87c7638e40d8ade2b8
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-06-05 16:15:38 -0400

    Update 10-CGN175.sh

--- 10-CGN175.sh
+++ 10-CGN175.sh
@@ -1,2 +1,5 @@
 # confirm bigip1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# confirm bigip2 is active, if this is an HA lab
+[[ "$LAB_NUMBER" =~ "-HA" ]] && for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
